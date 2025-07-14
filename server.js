0a1ae8d8c121805f74eafc0e51f703f287bcc770
TrilhaX <joaotrilha01@gmail.com>
2025-07-14 18:14:41 -0300

    Feito mais coisa

--- server.js
+++ server.js
@@ -2,8 +2,30 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
+// Middleware para entender JSON
+app.use(express.json());
+
+app.get('/users', (req, res) => {
     res.send('Servidor funcionando!');
+});
+
+app.post('/users/register', (req, res) => {
+    const { nome, email, senha, telefone } = req.body;
+
+    // Mostrar o JSON recebido no console
+    console.log('JSON recebido:', req.body);
+
+    if (!nome || !email || !senha || !telefone) {
+        return res.status(400).json({ erro: 'Todos os dados são obrigatórios' });
+    }
+
+    console.log(`Novo usuário: ${nome} - ${email}, ${telefone}, ${senha}`);
+
+    
+    res.status(201).json({
+        mensagem: 'Usuário criado com sucesso',
+        usuario: { nome, email }
+    });
 });
 
 app.listen(PORT, () => {
