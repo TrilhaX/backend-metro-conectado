fb72403e5a3166205268dec0c5799858b9b426cd
TrilhaX <joaotrilha01@gmail.com>
2025-08-29 12:08:27 -0300

    updated again

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ import { getAllUsers, getUserById, updatePerfilUser, register, login } from '../
 const router = express.Router()
 
 // Rotas de autenticação e usuários
-router.get('/', getAllUsers)
+router.get('/users', getAllUsers)
 router.get('/:id', getUserById)
 router.put('/:id', updatePerfilUser);
 router.post('/register', register)
