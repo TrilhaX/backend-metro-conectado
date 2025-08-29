3e2b56ab817fe683abba68c11bedb8233f913fc9
TrilhaX <joaotrilha01@gmail.com>
2025-08-29 12:02:06 -0300

    Updated to use in localhost

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import userRoutes from './routes/auth.js';
+import cors from 'cors';
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/user', userRoutes);
 
-app.listen(5051, () => {
-    console.log('Servidor rodando na porta 5051');
-});
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+app.use(express.json());
